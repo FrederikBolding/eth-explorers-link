edb98c4d12d33c92f457a1b937249635f2aa9aad
Frederik Bolding <frederik.bolding@gmail.com>
2019-07-29 01:14:19 +0200

    Updated config, Cleaned up layout

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -4,4 +4,6 @@ export const supportedExplorers = { Etherscan: (net, type, hash) => {
     return "https://blockscout.com/eth/" + net + "/" + type + "/" + hash;
 } }
 
-export const validTypes = ["tx", "address", "block", "token"]
+export const netNames = {"mainnet": "Mainnet", "rinkeby": "Rinkeby", "ropsten": "Ropsten", "goerli": "Goerli", "kovan": "Kovan" }
+
+export const typeNames = {"tx": "Transaction", "address": "Address", "block": "Block", "token": "Token"}
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,14 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Link from "../components/link"
 
-import { validTypes, supportedExplorers } from "../helpers/constants";
+import { netNames, typeNames, supportedExplorers } from "../helpers/constants"
+import MadeWithLove from "../components/MadeWithLove"
 
 class IndexPage extends Component {
   componentDidMount() {
     let split = this.props["*"].split("/")
     let data = { net: split[0], type: split[1], hash: split[2] }
-    this.setState({ data: data, valid: validTypes.indexOf(data.type) !== -1 })
+    this.setState({ data: data, valid: data.net in netNames && data.type in typeNames && data.hash.length > 0 })
   }
 
   componentWillUnmount() {}
@@ -24,14 +25,29 @@ class IndexPage extends Component {
   render() {
     return (
       <Layout>
-        <SEO title="Home" />
+        <SEO title={this.state.data ? netNames[this.state.data.net] + " " + typeNames[this.state.data.type] : ""} />
+        <h1>Ethereum Explorer List</h1>
         {!this.state.valid ? (
           <h1>Invalid</h1>
         ) : (
-          Object.keys(supportedExplorers).map((explorer, i) => (
-            <Link name={explorer} buildURL={supportedExplorers[explorer]} data={this.state.data} />
-          ))
+          <>
+            <h2>
+              {netNames[this.state.data.net]} {typeNames[this.state.data.type]}
+            </h2>
+            <h3>{this.state.data.hash}</h3>
+            {Object.keys(supportedExplorers).map((explorer, i) => (
+              <Link
+                name={explorer}
+                buildURL={supportedExplorers[explorer]}
+                data={this.state.data}
+              />
+            ))}
+          </>
         )}
+        <MadeWithLove
+          by="Frederik Bolding"
+          link="https://frederikbolding.com"
+        />
       </Layout>
     )
   }
